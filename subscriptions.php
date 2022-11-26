210e94c5812532c2b065f042cf328b625fcb9cad
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2022-11-26 23:21:33 +0800

    Add DB alert and conditional formatting

--- subscriptions.php
+++ subscriptions.php
@@ -3,78 +3,91 @@ Date: 21/11/2022
 Assessment Task Three (Team Project)
 Member Subscription Page -->
 <!-- START PHP -->
-<?php 
+<?php
 session_start();
-if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] == false) {
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] == false) {
     header("location: login.php");
     exit;
 }
 $newsletter = $newsflash = '';
 require_once('connection.php');
 $subscription_err = '';
- //include our connection
- $database = new Connection();
- $db = $database->open();
- try{	
-     $sql = "SELECT Newsletter, Newsflash FROM MembershipDatabase WHERE Email = ".$_SESSION['email'];
-     if ($stmt = $db->prepare($sql)){
-         if($stmt->execute()){
-             if($stmt->rowCount() == 1){
-                 $row = $stmt->fetch();
-                 $newsletter = $row["Newsletter"];
-                 $newsflash = $row["Newsflash"];
-             }
-         }
-     }
-  }
-  catch(PDOException $e){
-      $subscription_err =  "Could not fetch subscriptions: " . $e->getMessage();
-  }
-  //close connection
-  $database->close();
+//include our connection
+$database = new Connection();
+$db = $database->open();
+try {
+    $sql = "SELECT Newsletter, Newsflash FROM MembershipDatabase WHERE Email = " . $_SESSION['email'];
+    if ($stmt = $db->prepare($sql)) {
+        if ($stmt->execute()) {
+            if ($stmt->rowCount() == 1) {
+                $row = $stmt->fetch();
+                $newsletter = $row["Newsletter"];
+                $newsflash = $row["Newsflash"];
+            }
+        }
+    }
+} catch (PDOException $e) {
+    $subscription_err =  "Could not fetch subscriptions: " . $e->getMessage();
+}
+//close connection
+$database->close();
 ?>
 <!-- FINISH PHP-->
 <!doctype html>
 <html lang="en">
+
 <head>
-<?php
-	include_once('head.php');
-?>
-<title>Manage Subscriptions | Acme Art Gallery</title>
+    <?php
+    include_once('head.php');
+    ?>
+    <title>Manage Subscriptions | Acme Art Gallery</title>
 </head>
+
 <body>
     <!-- Grey with black text-->
     <?php
-	  include_once('inc_nav.php');
+    include_once('inc_nav.php');
     ?>
-    <h1>Hello, <?php echo $_SESSION["name"]; ?></h1>
-    <!--Start of table.-->
+    <h1>Subscription Settings</h1>
     <div class="container-fluid" id="containerStyle">
-        <h4>Subscription Settings</h4>
-        <p>Here you can adjust your active subscriptions for <?php echo $_SESSION["email"]; ?>:</p>
-      <!--Testing Search bar Start, Added action attribte for CrudTest....1) Original File name="InsertValuesTest.php, -->
-        <form class="d-flex mt-3" action="updateSubscription.php" method="POST" enctype="multipart/form-data">
-        <div class="mb-3">
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="checkNewsletter" <?php echo ($newsletter == 'y') ? ' checked' : ''; ?>>
-                <label class="form-check-label" for="checkNewsletter">Monthly newsletter</label>
+        <?php
+        if (!empty($subscription_err)) {
+        ?>
+            <div class="alert alert-danger" role="alert">
+                <?php echo $subscription_err; ?>
             </div>
-        </div>
-        <div class="mb-3">
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="checkNewsflash" <?php echo ($newsflash == 'y') ? ' checked' : ''; ?>>
-                <label class="form-check-label" for="checkNewsflash">Breaking news updates</label>
-            </div>
-        </div>
-        <button class="btn btn-success" type="submit">Update subscription</button>
-        </form>
-        <h4>Unsubscribe</h4>
-        <p>If you no longer wish to receive any updates from Acme Art Gallery, please click Unsubscribe</p>
-        <form class="d-flex mt-3" action="unsubscribe.php" method="POST">
-            <button class="btn"></button>
-        </form>
-<!--Search Bar Finish -->
-  </div>
+        <?php
+        } else {
+        ?>
+            <h4>Hello, <?php echo $_SESSION["name"]; ?></h4>
+            <p>Here you can adjust your active subscriptions for <?php echo $_SESSION["email"]; ?>:</p>
+            <!--Testing Search bar Start, Added action attribte for CrudTest....1) Original File name="InsertValuesTest.php, -->
+            <form class="d-flex mt-3" action="updateSubscription.php" method="POST" enctype="multipart/form-data">
+                <div class="mb-3">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="checkNewsletter" <?php echo ($newsletter == 'y') ? ' checked' : ''; ?>>
+                        <label class="form-check-label" for="checkNewsletter">Monthly newsletter</label>
+                    </div>
+                </div>
+                <div class="mb-3">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="checkNewsflash" <?php echo ($newsflash == 'y') ? ' checked' : ''; ?>>
+                        <label class="form-check-label" for="checkNewsflash">Breaking news updates</label>
+                    </div>
+                </div>
+                <button class="btn btn-success" type="submit">Update subscription</button>
+            </form>
+            <h4>Unsubscribe</h4>
+            <p>If you no longer wish to receive any updates from Acme Art Gallery, please click Unsubscribe</p>
+            <form class="d-flex mt-3" action="unsubscribe.php" method="POST">
+                <button class="btn"></button>
+            </form>
+        <?php
+        }
+        ?>
+
+        <!--Search Bar Finish -->
+    </div>
     <?php
     include_once('footer.php');
     ?>
