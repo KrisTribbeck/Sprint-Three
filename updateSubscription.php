36e9c7c59445547bf37a1bf0487bb4f8ae20aa0e
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2022-11-28 10:49:48 +0800

    Add button functions to update page

--- updateSubscription.php
+++ updateSubscription.php
@@ -9,3 +9,36 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] == false) {
     exit;
 }
 ?>
+<!doctype html>
+<html lang="en">
+<!--Head START. Replace with include_once(head.php) when uploading to server. -->
+<?php
+$pageName = "Update Subscriptions";
+include_once("head.php")
+?>
+<!--HEAD FINISH -->
+
+<body>
+    <!--NAV BAR START. Replace with include_once(inc_nav.php) when uploading to server.-->
+    <?php
+    include_once("inc_nav.php")
+    ?>
+    <!--NAV BAR FINISH. Replace with include_once(inc_nav.php) when uploading to server.-->
+    <!--Into page START.-->
+    <div class="container-fluid" id="containerStyle">
+        <div class="p-3 my-3 border border-dark rounded">
+            <h2>Update Subscriptions</h2>
+            <p>Subscriptions for <?php echo $_SESSION["email"]; ?> have successfully been updated</p>
+            <a class="btn btn-success" href="index.php" role="button">Return home</a>
+            <a class="btn btn-secondary" href="subscriptions.php" role="button">Manage subscriptions</a>
+        </div>
+    </div>
+    <!--Into page FINISH.-->
+    <!--FOOTER START. Replace with include_once(footer.php) when uploading to server. -->
+    <?php
+    include_once("footer.php")
+    ?>
+    <!--FOOTER END -->
+</body>
+
+</html>
