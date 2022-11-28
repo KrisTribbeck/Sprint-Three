b47c299d63aef43c6c4c66aa60976bc07b4553e5
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2022-11-29 00:03:57 +0800

    Bug fix delete error

--- delete-user.php
+++ delete-user.php
@@ -14,7 +14,8 @@ if (isset($_POST["id"]) && !empty($_POST["id"])) {
     $database = new Connection();
     $db = $database->open();
     try {
-        $sql = "DELETE FROM MembershipDatabase WHERE Id = :id";
+        $param_id = trim($_POST["id"]);
+        $sql = "DELETE FROM MembershipDatabase WHERE Id = $param_id";
         if ($stmt = $db->prepare($sql)) {
             if ($stmt->execute()) {
                 header("location: admin.php");
@@ -28,7 +29,7 @@ if (isset($_POST["id"]) && !empty($_POST["id"])) {
     }
     $database->close();
 } else {
-    if(empty(trim($_GET["id"]))){
+    if (empty(trim($_GET["id"]))) {
         header("location: error.php");
         exit();
     }
@@ -61,14 +62,12 @@ if (isset($_POST["id"]) && !empty($_POST["id"])) {
             }
             ?>
             <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                <div class="alert alert-danger">
-                    <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>" />
-                    <p>Are you sure you want to delete this user from the mailing list?</p>
-                    <p>
-                        <input type="submit" value="Yes" class="btn btn-danger">
-                        <a href="admin.php" class="btn btn-secondary ml-2">No</a>
-                    </p>
-                </div>
+                <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>">
+                <p>Are you sure you want to delete this user from the mailing list?</p>
+                <p>
+                    <input type="submit" value="Yes" class="btn btn-danger">
+                    <a href="admin.php" class="btn btn-secondary ml-2">No</a>
+                </p>
             </form>
         </div>
     </div>
