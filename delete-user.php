e69bd80f87afacfd709abb86a8f583f7e5eef25b
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2022-11-28 23:45:19 +0800

    Add form contents to delet user page

--- delete-user.php
+++ delete-user.php
@@ -25,6 +25,16 @@ if (isset($_POST["id"]) && !empty($_POST["id"])) {
     <div class="container-fluid" id="containerStyle">
         <div class="p-3 my-3 border border-dark rounded">
             <h2>Delete User</h2>
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                <div class="alert alert-danger">
+                    <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>"/>
+                    <p>Are you sure you want to delete this user from the mailing list?</p>
+                    <p>
+                        <input type="submit" value="Yes" class="btn btn-danger">
+                        <a href="admin.php" class="btn btn-secondary ml-2">No</a>
+                    </p>
+                </div>
+            </form>
         </div>
     </div>
 </body>
