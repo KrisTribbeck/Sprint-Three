ed5de4cbbaca798d5348263c2168f9fa1de1d748
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2022-11-28 23:38:41 +0800

    Add content to delet user page

--- delete-user.php
+++ delete-user.php
@@ -1,12 +1,28 @@
 <?php
-if(isset($_POST["id"]) && !empty($_POST["id"])){
+if (isset($_POST["id"]) && !empty($_POST["id"])) {
     require_once("connection.php");
     $sql = "DELETE FROM MembershipDatabase WHERE Id = :id";
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <?php include_once("head.php")?>
-    </head>
+
+<head>
+    <?php
+    $pageName = "Delete User";
+    include_once("head.php");
+    ?>
+</head>
+
+<body>
+    <?php
+    include_once("inc_nav.php");
+    ?>
+    <div class="container-fluid" id="containerStyle">
+        <div class="p-3 my-3 border border-dark rounded">
+            <h2>Delete User</h2>
+        </div>
+    </div>
+</body>
+
 </html>
