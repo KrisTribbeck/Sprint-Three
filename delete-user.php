b47c26999ab740b1814cb2a7742292e75f6fdc2d
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2022-11-28 23:55:04 +0800

    Add error trapping to delete form

--- delete-user.php
+++ delete-user.php
@@ -4,8 +4,24 @@ Assessment Task Three (Team Project)
 Delete User Page -->
 <?php
 if (isset($_POST["id"]) && !empty($_POST["id"])) {
+    $delete_err = '';
     require_once("connection.php");
-    $sql = "DELETE FROM MembershipDatabase WHERE Id = :id";
+    $database = new Connection();
+    $db = $database->open();
+    try {
+        $sql = "DELETE FROM MembershipDatabase WHERE Id = :id";
+        if ($stmt = $db->prepare($sql)) {
+            if ($stmt->execute()) {
+                header("location: admin.php");
+                exit();
+            } else {
+                $delete_err = "Could not delete user.";
+            }
+        }
+    } catch (PDOException $e) {
+        $delete_err = "Could not delete user: " + $e->getMessage();
+    }
+    $database->close();
 }
 ?>
 <!DOCTYPE html>
@@ -25,9 +41,18 @@ if (isset($_POST["id"]) && !empty($_POST["id"])) {
     <div class="container-fluid" id="containerStyle">
         <div class="p-3 my-3 border border-dark rounded">
             <h2>Delete User</h2>
+            <?php
+            if (!empty($delete_err)) {
+            ?>
+                <div class="alert alert-danger" role="alert">
+                    <?php echo $delete_err; ?>
+                </div>
+            <?php
+            }
+            ?>
             <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                 <div class="alert alert-danger">
-                    <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>"/>
+                    <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>" />
                     <p>Are you sure you want to delete this user from the mailing list?</p>
                     <p>
                         <input type="submit" value="Yes" class="btn btn-danger">
